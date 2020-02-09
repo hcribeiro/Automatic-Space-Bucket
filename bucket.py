9273076012a8349309555033b8e115265247628e
Hugo Ribeiro <hugo.ribeiro@comcast.net>
2020-02-09 03:09:25 -0800

    Added the sensor and adc

--- bucket.py
+++ bucket.py
@@ -1,5 +1,4 @@
 import sys, os, time, datetime, board, busio
-import RPi.GPIO as GPIO
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 from gpiozero import LED, DigitalOutputDevice, SmoothedInputDevice
@@ -12,7 +11,6 @@ startTime = datetime.datetime.now().time()
 endTime = (datetime.datetime.now() + datetime.timedelta(hours=23)).time()
 
 def main():
-    GPIO.setmode(GPIO.BCM)
     i2c = busio.I2C(board.SCL, board.SDA)
     ads = ADS.ADS1115(i2c)
     led = LED(LIGHT_PIN, active_high=False)
@@ -24,30 +22,28 @@ def main():
         now = datetime.datetime.now().time() 
         if (time_in_range(startTime, endTime, now)):
             led.on()
-            fans.on()
-    
+            fans.on()    
             # Read from sensor
-#            if (reading < 2500):
-#                pumps.on()
-#                print("Pump turned on!")
-#            else:
-#                pumps.off()
-#                print("Pump turned off!")
+            if (sensor.value < 2500):
+                pumps.on()
+                print("Pump turned on!")
+            else:
+                pumps.off()
+                print("Pump turned off!")
             print("LED turned on!")
-            print("Time Now: " + str(now))
-            print("Start Time: " + str(startTime))
-            print("End Time: " + str(endTime))
-            print("Sensor: " + str(sensor.value))
         elif not (time_in_range(startTime, endTime, now)):
             led.off()
             fans.off()
             print("LED turned off!")
-            print("Time Now: " + str(now))
-            print("Start Time: " + str(startTime))
-            print("End Time: " + str(endTime))
+        print("Time Now: " + str(now))
+        print("Start Time: " + str(startTime))
+        print("End Time: " + str(endTime))
+        print("Sensor: " + str(sensor.value))
         time.sleep(10)
+        
 
 def time_in_range(startTime, endTime, nowTime):
+    """Returns bool dependant on if nowTime is between startTime and endTime"""
     if startTime < endTime:
         return nowTime >= startTime and nowTime <= endTime
     else: #Over midnight

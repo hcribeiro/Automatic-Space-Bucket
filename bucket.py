b09f7cc43977f55cb5eb1eccdef4b5abbdd64d06
hcribeiro <hugo.ribeiro@comcast.net>
2020-02-08 20:22:16 -0800

    Added adafruit ADS1115

--- bucket.py
+++ bucket.py
@@ -1,28 +1,43 @@
-import sys, os, time, datetime
+import sys, os, time, datetime, board, busio
+import RPi.GPIO as GPIO
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 from gpiozero import LED, DigitalOutputDevice, SmoothedInputDevice
 
 LIGHT_PIN = 26
 FANS_PIN = 20
 PUMP_PIN = 21
-SENSOR_PIN = 16
 
 startTime = datetime.datetime.now().time()
 endTime = (datetime.datetime.now() + datetime.timedelta(hours=23)).time()
 
 def main():
+    GPIO.setmode(GPIO.BCM)
+    i2c = busio.I2C(board.SCL, board.SDA)
+    ads = ADS.ADS1115(i2c)
     led = LED(LIGHT_PIN, active_high=False)
     fans = DigitalOutputDevice(FANS_PIN, active_high=False)
     pumps = DigitalOutputDevice(PUMP_PIN, active_high=False)
+    sensor = AnalogIn(ads, ADS.P0)
     while(True):
         os.system('clear')
         now = datetime.datetime.now().time() 
         if (time_in_range(startTime, endTime, now)):
             led.on()
             fans.on()
+    
+            # Read from sensor
+#            if (reading < 2500):
+#                pumps.on()
+#                print("Pump turned on!")
+#            else:
+#                pumps.off()
+#                print("Pump turned off!")
             print("LED turned on!")
             print("Time Now: " + str(now))
             print("Start Time: " + str(startTime))
             print("End Time: " + str(endTime))
+            print("Sensor: " + str(sensor.value))
         elif not (time_in_range(startTime, endTime, now)):
             led.off()
             fans.off()
